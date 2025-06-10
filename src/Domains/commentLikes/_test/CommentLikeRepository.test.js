39b9b710e58d93624650acde28fd8026526da994
AnasPunggawa <anaspunggawa8882@gmail.com>
2025-06-10 13:33:06 +0800

    fix(Domains): modifying CommentLikeRepository.test.js

--- src/Domains/commentLikes/_test/CommentLikeRepository.test.js
+++ src/Domains/commentLikes/_test/CommentLikeRepository.test.js
@@ -8,6 +8,6 @@ describe('CommentLikeRepository interface', () => {
     // Action & Assert
     await expect(
       commentLikeRepository.commentLikeToggle('', '')
-    ).rejects.toThrow('COMMENT_LIKES_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    ).rejects.toThrow('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
